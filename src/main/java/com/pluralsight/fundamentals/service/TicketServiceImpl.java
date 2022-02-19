e07b75d9849a37656ad32e927389a99ddd57e776
Rade Srdanovic <w.radesrdan@gmail.com>
2022-02-20 00:02:28 +0100

    Updated the index.html image tag

---
+++ src/main/java/com/pluralsight/fundamentals/service/TicketServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pluralsight.fundamentals.service;
+
+import com.pluralsight.fundamentals.entity.Ticket;
+import com.pluralsight.fundamentals.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    public Iterable<Ticket> listTickets() {
+        return ticketRepository.findAll();
+    }
+
+}
