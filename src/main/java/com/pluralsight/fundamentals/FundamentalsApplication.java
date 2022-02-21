6a7b6dc65c4316dfa642c15d986691572872ba7d
Rade Srdanovic <w.radesrdan@gmail.com>
2022-02-21 01:13:08 +0100

    - Removed Services - Removed Controller - Implemented GraphQL - Renamed data.sql - Implemented Dependencies - Added Mutations - Added Resolver - Added application.graphqls

--- src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
+++ src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -1,13 +1,7 @@
 package com.pluralsight.fundamentals;
-import com.pluralsight.fundamentals.entity.Application;
-import com.pluralsight.fundamentals.repository.ApplicationRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FundamentalsApplication {
--- src/test/java/com/pluralsight/fundamentals/FundamentalsApplicationTests.java
+++ src/test/java/com/pluralsight/fundamentals/FundamentalsApplicationTests.java
@@ -2,10 +2,7 @@ package com.pluralsight.fundamentals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class FundamentalsApplicationTests {
 
